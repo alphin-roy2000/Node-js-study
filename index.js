86c1d56759c616f2a9ffb0253ee79e7d24e1ec40
Alphin Roy <alphinroy430@gmail.com>
2020-08-09 15:51:44 +0530

    .gitignore included for not commiting node_modules

---
+++ index.js
@@ -0,0 +1,8 @@
+var _ = require('underscore');//underscore.js
+
+//core module          ///[ order of search]
+//file or folder
+//node_modules
+
+var result = _.contains([1,2,3],2);
+console.log(result);
